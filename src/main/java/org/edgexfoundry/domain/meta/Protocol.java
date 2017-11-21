5337788266486c490ddf7bb7f98dda5c37f1aa97
chadyoungdell <chadbyoung@gmail.com>
2017-11-21 11:27:55 -0600

    added comments for Modbus and SNMP
    Signed-off-by: chadyoungdell <chadbyoung@gmail.com>

--- src/main/java/org/edgexfoundry/domain/meta/Protocol.java
+++ src/main/java/org/edgexfoundry/domain/meta/Protocol.java
@@ -20,10 +20,11 @@ package org.edgexfoundry.domain.meta;
 
 // TODO - someday, we may need to refactor addressable based on needs of protocol. Other is a
 // placeholder today.
-// HTTP - for REST communications
+// HTTP - Can be used for REST communications and SNMP
 // TCP - for MQTT and other general TCP based communications
 // MAC - MAC address - low level (example serial) communications
 // ZMQ - Zero MQ communications
+// OTHER - Can be used for Modbus
 // SSL - for TLS encrypted sockets
 public enum Protocol {
   HTTP, TCP, MAC, ZMQ, OTHER, SSL
